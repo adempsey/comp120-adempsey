c58167ae2c87ea1615a90cceed162014736ecc1d
adempsey <amd0331@gmail.com>
2013-09-30 16:14:45 -0400

    validates username for character set

--- a2/messagehub/app/models/message.rb
+++ a2/messagehub/app/models/message.rb
@@ -1,5 +1,8 @@
 class Message < ActiveRecord::Base
-	validates :username, presence: true
-	validates :content, presence: true
+	validates :username, 
+		presence: true, 
+		length: {minimum: 3, maximum: 60}
+	validates_format_of :username, :with => /\A[a-zA-Z\-\_]*\z/
+	validates :content, presence: true, length: {maximum: 160}
 	validates :app_id, presence: true
 end
