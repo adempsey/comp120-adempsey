d94ea75548d6addb1f54b3fd6212edd15b392ff2
adempsey <amd0331@gmail.com>
2013-10-01 13:39:51 -0400

    timeago used and validates app

--- a2/messagehub/app/models/app.rb
+++ a2/messagehub/app/models/app.rb
@@ -1,2 +1,4 @@
 class App < ActiveRecord::Base
+	validates :app_title, presence: true, length: {minimum: 1}
+	validates :description, presence: true, length: {minimum: 1}
 end
--- a2/messagehub/app/models/message.rb
+++ a2/messagehub/app/models/message.rb
@@ -3,6 +3,6 @@ class Message < ActiveRecord::Base
 		presence: true, 
 		length: {minimum: 3, maximum: 60}
 	validates_format_of :username, :with => /\A[a-zA-Z\-\_]*\z/
-	validates :content, presence: true, length: {maximum: 160}
-	validates :app_id, presence: true
+	validates :content, presence: true, length: {minimum: 1, maximum: 160}
+	validates :app_id, presence: true, length: {minimum: 1}
 end
