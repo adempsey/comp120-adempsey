d5b2b5fe89fe6ebdf9fd5e61e1a2b40b7c0e03ad
adempsey <amd0331@gmail.com>
2013-10-03 19:47:49 -0400

    updated seeds file

--- a2/messagehub/db/seeds.rb
+++ a2/messagehub/db/seeds.rb
@@ -1,4 +1,4 @@
-App.create({:app_title => 'ios', :description => 'From iOS app'})
-App.create({:app_title => 'android', :description => 'From Android app'})
-App.create({:app_title => 'web', :description => 'From default web app'})
-App.create({:app_title => 'unknown', :description => 'From an unknown source'})
+App.create({:app_title => 'ios', :description => 'From iOS app', :active => true})
+App.create({:app_title => 'android', :description => 'From Android app', :active => true})
+App.create({:app_title => 'web', :description => 'From default web app', :active => true})
+App.create({:app_title => 'unknown', :description => 'From an unknown source', :active => true})
