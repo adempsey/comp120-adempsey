9eb402c4e7da70f1621d8f28d0fac8238d1547d7
adempsey <amd0331@gmail.com>
2013-11-14 20:29:19 -0500

    changed messagesTableView activity indicator to networkActivityIndicator

--- mobileapp/mobileapp/messagesTableViewController.m
+++ mobileapp/mobileapp/messagesTableViewController.m
@@ -25,18 +25,8 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-	[self.messages removeAllObjects];
-	[self.tableView reloadData];
 	
-	__block UIActivityIndicatorView* indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
-	[indicator startAnimating];
-	indicator.frame = CGRectMake(
-								 self.view.frame.size.width/2 - indicator.frame.size.width+12,
-								 self.view.frame.size.height/2+48,
-								 indicator.frame.size.width,
-								 indicator.frame.size.height);
-	indicator.autoresizingMask = UIViewAutoresizingNone;
-	[self.view addSubview:indicator];
+	[UIApplication sharedApplication].networkActivityIndicatorVisible = TRUE;
 	
     NSMutableURLRequest* request = [[NSMutableURLRequest alloc] init];
     [request setURL:[NSURL URLWithString:@"http://0.0.0.0:3000/messages.json"]];
@@ -46,11 +36,14 @@
                                        queue:[[NSOperationQueue alloc] init]
                            completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
                                if (connectionError) {
-                                   NSLog(@"Error");
-                               } else {
+								   dispatch_async(dispatch_get_main_queue(), ^{
+									   NSLog(@"Error");
+									   [UIApplication sharedApplication].networkActivityIndicatorVisible = FALSE;
+								   });
+							   } else {
                                    self.messages = [NSMutableArray arrayWithArray:[NSJSONSerialization JSONObjectWithData:data options:0 error:nil]];
 								   dispatch_async(dispatch_get_main_queue(), ^{
-									   [indicator removeFromSuperview];
+									   [UIApplication sharedApplication].networkActivityIndicatorVisible = FALSE;
 									   [self.tableView reloadData];
 								   });
                                }
